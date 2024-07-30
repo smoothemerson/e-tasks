1c3f74198bc103f9c94578ae6ed67ff12c17f5ed
emerson <emerson8096@gmail.com>
2024-07-30 08:15:17 -0300

    change img and name of the profile

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -17,10 +17,10 @@ export function SideMenu() {
       </Nav>
 
       <Footer>
-        <img src="https://github.com/rodrigorgtic.png" alt="Foto do usuário" />
+        <img src="https://github.com/smoothemerson.png" alt="Foto do usuário" />
         <div>
-          <strong>Rodrigo Gonçalves</strong>
-          <small>rodrigo@email.com</small>
+          <strong>Emerson rocha</strong>
+          <small>emerson@email.com</small>
         </div>
 
         <Button type="button">
